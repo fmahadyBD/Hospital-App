6357173da78d53ffda59bd7b26e8909997b85fef
fmahadyBD <fmahady01@gmail.com>
2025-06-03 07:37:01 +0600

    Initial Commit

---
+++ hospital-services/src/test/java/com/fmahadybd/hospital_services/HospitalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmahadybd.hospital_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
